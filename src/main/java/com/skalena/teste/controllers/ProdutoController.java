19aedde19d939f0bdcc15f8b79b531e69c686b27
fredmartinseca <fredmartinseca@gmail.com>
2019-08-02 00:22:24 -0300

    Instaciando bd

--- src/main/java/com/skalena/teste/controllers/ProdutoController.java
+++ src/main/java/com/skalena/teste/controllers/ProdutoController.java
@@ -1,9 +1,28 @@
 package com.skalena.teste.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.skalena.teste.entity.Produto;
+import com.skalena.teste.service.IProdutoService;
 
 @RestController
 public class ProdutoController {
 	
+	@Autowired
+	private IProdutoService produtoService;
+	
+	@GetMapping("/listar")
+	public List<Produto> listar() {
+		return produtoService.findAll();
+	}
+	@GetMapping("/ver/{id}")
+	public Produto detalhe(@PathVariable Long id) {
+		return produtoService.findById(id);
+	}
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into produto (nome, descricao, quantidade, preco, data) values('P1', 'tipo 1', 5, 3.9, now());
+insert into produto (nome, descricao, quantidade, preco, data) values('P2', 'tipo 2', 1, 6.9, now());
+insert into produto (nome, descricao, quantidade, preco, data) values('P3', 'tipo 3', 6, 12.9, now());
+insert into produto (nome, descricao, quantidade, preco, data) values('P4', 'tipo 4', 8, 1.2, now());
+insert into produto (nome, descricao, quantidade, preco, data) values('P5', 'tipo 5', 12, 9.9, now());
+insert into produto (nome, descricao, quantidade, preco, data) values('P6', 'tipo 6', 7, 8.9, now());
