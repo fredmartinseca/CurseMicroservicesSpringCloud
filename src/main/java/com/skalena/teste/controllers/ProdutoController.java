3a18ab5d8d9413b838c475fa7f6e29b4ed0c4c1e
fredmartinseca <fredmartinseca@gmail.com>
2019-08-01 22:07:28 -0300

    Anotações na class produto e implementação de repository e service

--- src/main/java/com/skalena/teste/controllers/ProdutoController.java
+++ src/main/java/com/skalena/teste/controllers/ProdutoController.java
@@ -1,8 +1,8 @@
 package com.skalena.teste.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RestController
 public class ProdutoController {
 	
 
